404183f70eca54a1733f93fb182e5936bc17e6fc
Oussama OUAZIZ <oussamaouaziz@gmail.com>
2020-04-18 18:47:29 +0200

    Ex 01 and 02

---
+++ ex01/ex01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*  Premier
+    commentaire
+    multi-lignes
+*/
+int main(int argc, char* argv[]) {
+    int distance = 100;
+
+    // Une deuxième façon d'écrire un commentaire mais cette fois sur une seule ligne.
+    printf("You are %d miles away.\n", distance);
+
+    return 0;
+}
